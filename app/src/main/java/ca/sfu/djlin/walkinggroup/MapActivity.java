f48d12be334b8978f9f44d5bd5fd3bf0d05ac3e7
Daniel <daniel.juiche@gmail.com>
2018-06-28 13:28:14 -0700

    Set up Google Maps API

---
+++ app/src/main/java/ca/sfu/djlin/walkinggroup/MapActivity.java
@@ -0,0 +1,17 @@
+package ca.sfu.djlin.walkinggroup;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity{
+
+    public static final String TAG = "MapActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
