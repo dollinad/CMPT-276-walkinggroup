874d7fc99be0d8c2a093b98190acaacabfdafd68
Harry <chengf@sfu.ca>
2018-07-03 22:12:36 -0700

    delete mainactivity

--- app/src/main/java/ca/sfu/djlin/walkinggroup/app/MainActivity.java
+++ app/src/main/java/ca/sfu/djlin/walkinggroup/app/MainActivity.java
@@ -5,13 +5,12 @@ import android.os.Bundle;
 
 import ca.sfu.djlin.walkinggroup.R;
 import ca.sfu.djlin.walkinggroup.dataobjects.Group;
-import ca.sfu.djlin.walkinggroup.dataobjects.GroupCollection;
 import ca.sfu.djlin.walkinggroup.model.User;
 
 public class MainActivity extends AppCompatActivity {
     private Group group;
     private User user;
-    private GroupCollection groupcollection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +21,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void ini() {
-        groupcollection=GroupCollection.getInstance();
-        System.out.println(groupcollection.getGroupSize());
-        System.out.println(groupcollection.getGroup(0).getName());
-        //System.out.println(groupcollection.getGroup(0));
-
     }
 }
